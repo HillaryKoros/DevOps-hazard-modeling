67f0a7d07e50270cb070f7fdc4c4bf1b0e384781
Nishadh KA <nishadhka@gmail.com>
2024-08-07 12:11:55 +0300

    updated the 01-pet process into perfect workflow

--- 01-pet-process-1km.py
+++ 01-pet-process-1km.py
@@ -1,76 +1,86 @@
-
+from prefect import flow, task
 from dotenv import load_dotenv
 import os
-from datetime import datetime, timedelta
-
-import pandas as pd
-import numpy as np
-import rioxarray
+from datetime import datetime
 import xarray as xr
-import xesmf as xe
-import geopandas as gp
 from dask.distributed import Client
 
-from utils import pet_list_files_by_date
-from utils import pet_download_extract_bilfile
-from utils import pet_bil_netcdf
-from utils import pet_find_missing_dates
-from utils import pet_read_netcdf_files_in_date_range
-from utils import pet_extend_forecast
-    
-from utils import make_zones_geotif
-from utils import get_dask_client_params
-
-from utils import process_zone_and_subset_data
-from utils import regrid_dataset
-
+from utils import (
+    pet_list_files_by_date,
+    pet_download_extract_bilfile,
+    pet_bil_netcdf,
+    pet_read_netcdf_files_in_date_range,
+    get_dask_client_params,
+    process_zone_and_subset_data,
+    regrid_dataset
+)
 
 load_dotenv()
 
-input_path=os.getenv("data_path")
-output_dir=f'{input_path}PET/dir/'
-netcdf_path=f'{input_path}PET/netcdf/'
+@task
+def setup_environment():
+    input_path = os.getenv("data_path")
+    output_dir = f'{input_path}PET/dir/'
+    netcdf_path = f'{input_path}PET/netcdf/'
+    params = get_dask_client_params()
+    client = Client(**params)
+    return input_path, output_dir, netcdf_path, client
 
-params = get_dask_client_params()
+@task
+def get_pet_files(url, start_date, end_date):
+    return pet_list_files_by_date(url, start_date, end_date)
 
-client = Client(**params)
+@task
+def process_pet_files(pet_list, output_dir, netcdf_path):
+    for file_url, date in pet_list:
+        pet_download_extract_bilfile(file_url, output_dir)
+        pet_bil_netcdf(file_url, date, output_dir, netcdf_path)
 
-start_date = datetime(2024, 4, 1)
-end_date = datetime(2024, 7, 22)
+@task
+def read_pet_data(netcdf_path, start_date, end_date):
+    pds = pet_read_netcdf_files_in_date_range(netcdf_path, start_date, end_date)
+    return pds.rename(x='lon', y='lat')
 
+@task
+def process_zone(input_path, pds, zone_str):
+    shapefl_name = f'{input_path}WGS/{zone_str}.shp'
+    km_str = 1
+    z1ds, pdsz1, zone_extent = process_zone_and_subset_data(shapefl_name, km_str, zone_str, pds)
+    return z1ds, pdsz1, zone_extent
 
-url = "https://edcintl.cr.usgs.gov/downloads/sciweb1/shared/fews/web/global/daily/pet/downloads/daily/"
+@task
+def regrid_pet_data(pds, input_chunk_sizes, output_chunk_sizes, zone_extent):
+    return regrid_dataset(
+        pds,
+        input_chunk_sizes,
+        output_chunk_sizes,
+        zone_extent,
+        regrid_method="bilinear"
+    )
 
-pet_list=pet_list_files_by_date(url, start_date, end_date)
+@flow
+def pet_processing_workflow(start_date: datetime, end_date: datetime, zone_str: str):
+    input_path, output_dir, netcdf_path, client = setup_environment()
+    
+    url = "https://edcintl.cr.usgs.gov/downloads/sciweb1/shared/fews/web/global/daily/pet/downloads/daily/"
+    pet_list = get_pet_files(url, start_date, end_date)
+    
+    process_pet_files(pet_list, output_dir, netcdf_path)
+    
+    pds = read_pet_data(netcdf_path, start_date, end_date)
+    
+    z1ds, pdsz1, zone_extent = process_zone(input_path, pds, zone_str)
+    
+    input_chunk_sizes = {'time': 10, 'lat': 30, 'lon': 30}
+    output_chunk_sizes = {'lat': 300, 'lon': 300}
+    
+    regridded_data = regrid_pet_data(pds, input_chunk_sizes, output_chunk_sizes, zone_extent)
+    
+    return regridded_data
 
-
-for file_url, date in pet_list:
-    pet_download_extract_bilfile(file_url,output_dir)
-    pet_bil_netcdf(file_url,date,output_dir,netcdf_path)
-
-pds=pet_read_netcdf_files_in_date_range(netcdf_path, start_date, end_date)
-pds = pds.rename(x='lon', y='lat')
-
-
-##############
-#zone 6
-shapefl_name=f'{input_path}WGS/zone6.shp'
-km_str=1
-zone_str='zone6'
-z1ds, pdsz1, zone_extent = process_zone_and_subset_data(shapefl_name, km_str, zone_str, pds)
-
-
-
-
-input_chunk_sizes = {'time': 10, 'lat': 30, 'lon': 30}
-output_chunk_sizes = {'lat': 300, 'lon': 300}
-
-# Call the function with the dictionary
-z1ds = regrid_dataset(
-    pds,
-    input_chunk_sizes,
-    output_chunk_sizes,
-    zone_extent,
-    regrid_method="bilinear"
-)
-
+if __name__ == "__main__":
+    start_date = datetime(2024, 4, 1)
+    end_date = datetime(2024, 7, 22)
+    zone_str = 'zone6'
+    
+    pet_processing_workflow(start_date, end_date, zone_str)
